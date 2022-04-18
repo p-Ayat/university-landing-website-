fe4477b4cb83883bfa4f1168b90368b38f4eeed8
Ayat <68519230+p-Ayat@users.noreply.github.com>
2022-04-19 01:58:42 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,12 @@
+const myList = document.querySelector("#my-list");
+const btn = document.querySelector("#page-header span");
+
+function toggle() {
+    if (myList.style.display = "none") {
+        myList.style.display = "flex";
+    } else {
+        myList.style.display = "none";
+    }
+}
+
+// btn.onclick = toggle();
